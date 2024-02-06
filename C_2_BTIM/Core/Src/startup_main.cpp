dd00d32af766fa548d6c088f5a46f927ec26d08e
Lihao <cgyfanqian@mail.com>
2024-02-06 21:15:38 +0800

    提交新工程

---
+++ C_2_BTIM/Core/Src/startup_main.cpp
@@ -0,0 +1,15 @@
+#include "startup_main.h"
+#include "bsp_delay.h"
+#include "tim.h"
+
+void startup_main(void)
+{
+
+	
+#if isRTOS==0    	//如果是裸机开发
+	for(;;)  //等同于while(true)
+	{
+		bsp_delay.f1.ms(500);
+	}
+#endif
+}
